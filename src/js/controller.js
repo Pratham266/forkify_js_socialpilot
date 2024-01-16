fe46e241ac633ac7558a569f8e5604aa9557f13c
Barot Pratham Janakkumar <barotpratham30@gmail.com>
2024-01-16 18:32:55 +0530

    first commit

---
+++ src/js/controller.js
@@ -0,0 +1,55 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+
+
+
+if(module.hot){
+  module.hot.accept();
+}
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    
+    if(!id) return;
+    recipeView.renderSpinner()
+    //1)loading a recipe
+    await model.loadRecipe(id);
+    
+    //2) rendering recipe
+    recipeView.render(model.state.recipe);
+  
+  } catch (err) {
+    console.log("error",err);
+    recipeView.renderError();
+  }
+};
+
+const controlSearchResults = async function(){
+  try{
+    resultsView.renderSpinner();
+    //1)get search query
+    const query  = searchView.getQuery();
+    if(!query) return;
+
+    //2)load search results
+    await model.loadSearchResults(query);
+
+    //3)render results
+    console.log(model.state.search.results);
+    resultsView.render(model.state.search.results)
+  }catch(err){
+    console.log(err);
+  }
+}
+
+
+const init = function(){
+  recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+}
+init();
